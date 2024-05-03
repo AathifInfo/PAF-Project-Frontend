13401992f67e70d062c296364e201e1f37f20fe9
AathifInfo <aathifinfo115@gmail.com>
2024-05-03 07:37:35 +0530

    Some logout function added

--- src/components/workour-plan-page/WorkourPlanPage.js
+++ src/components/workour-plan-page/WorkourPlanPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import MiddleWorkoutPlan from "../middleworkoutplan/MiddleWorkoutPlan";
 import Left from "../left/Left";
 import Right from "../right/Right";
@@ -8,38 +8,74 @@ import { toast } from "react-toastify";
 import { useLocation, useNavigate } from "react-router-dom";
 import { getAllWorkoutPlans } from "../../util/APIUtils";
 import WorkoutPlanForm from "../right-workout-plan-form/WorkoutPlanForm";
+import { getCurrentUser } from "../../util/APIUtils";
+import { ACCESS_TOKEN, USER_EMAIL, USER_NAME } from "../../constants";
+import LoadingIndicator from "../../common/LoadingIndicator";
 
-export default function WorkourPlanPage({ authenticated, onLogout }) {
-  // const [workoutPlans, setWorkoutPlans] = useState([]);
 
-  // const fetchAllPost = async () => {
-  //   try {
-  //     const response = await getAllWorkoutPlans();
-  //     setWorkoutPlans(response.data);
-  //     console.log("fecthed success")
-  //     console.log(response.data)
-  //     console.log(workoutPlans) 
-  //   } catch (error) {
-  //     console.log("fecthed failed")
-  //     console.log(error)
-  //     toast("Oops something went wrong!", { type: "error",position: "bottom-right",
-  //       autoClose: 5000,
-  //       hideProgressBar: false,
-  //       closeOnClick: true,
-  //       pauseOnHover: true,
-  //       draggable: true,
-  //       progress: undefined,
-  //       theme: "dark", });
-  //   }
-  // };
+export default function WorkourPlanPage() {
+  const [state, setState] = useReducer(
+    (prevState, newState) => {
+      return { ...prevState, ...newState };
+    },
+    {
+      authenticated: true,
+      currentUser: null,
+      loading: true,
+    }
+  );
 
-  // useEffect(() => {
-  //   fetchAllPost();
-  // }, []);
+  const loadCurrentlyLoggedInUser = () => {
+    getCurrentUser()
+      .then((response) => {
+        setState({
+          currentUser: response,
+          authenticated: false,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        setState({
+          loading: false,
+        });
+      });
+  };
+
+
+  const handleLogout = () => {
+    console.log("Handle logout is triggered")
+    localStorage.removeItem(ACCESS_TOKEN);
+    localStorage.removeItem(USER_EMAIL);
+    localStorage.removeItem(USER_NAME);
+    setState({
+      authenticated: false,
+      currentUser: null,
+    });
+    toast("You're safely logged out!", {
+      type: "success",
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+    // navigate("/login");
+  };
+
+  useEffect(() => {
+    loadCurrentlyLoggedInUser();
+  }, []);
+
+  if (state.loading) {
+    return <LoadingIndicator />;
+  }
 
   return (
     <div>
-      <Header authenticated={authenticated} onLogout={onLogout} />
+      <Header authenticated={state.authenticated} onLogout={handleLogout} />
       <main>
         <div className="container">
           <Left />
